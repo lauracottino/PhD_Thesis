ed7dcff21c03b18427c660f84bc7c312c46eefbc
Laura Cottino <40057241+lauracottino@users.noreply.github.com>
2022-02-21 17:08:48 +0200

    Update AF0.py

--- Chapter 2 scripts/AF0.py
+++ Chapter 2 scripts/AF0.py
@@ -5,5 +5,5 @@ fname = sys.argv[1]
 df = pd.read_csv(fname,delimiter="\t")
 want = df[df['AF']==0]
 to_print = (want[['CHROM','POS']])
-to_print.to_csv('/spaces/emma/MantaResults/AF0_to_exclude.tsv',sep='\t')
+to_print.to_csv('AF0_to_exclude.tsv',sep='\t')
 #print(df)
