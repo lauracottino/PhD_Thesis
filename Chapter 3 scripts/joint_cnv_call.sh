8c26b7f680202cd7e792ff9642b31d83812d8fb7
Laura Cottino <40057241+lauracottino@users.noreply.github.com>
2022-02-21 17:20:46 +0200

    Update joint_cnv_call.sh

--- Chapter 3 scripts/joint_cnv_call.sh
+++ Chapter 3 scripts/joint_cnv_call.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #SBATCH --mem=50gb
 
-ref_seq="/dataB/aux/38/Homo_sapiens_assembly38.fasta"
-sig1="/spaces/lcottino/Pacbio/pbsv/vcfs/CFF0X_combined/CFF0X.svsig.gz"
-sig2="/spaces/lcottino/Pacbio/pbsv/vcfs/CWF0F/CWF0F.svsig.gz"
-sig3="/spaces/lcottino/Pacbio/pbsv/vcfs/CNV0L/CNV0L.svsig.gz"
-sig4="/spaces/lcottino/Pacbio/pbsv/vcfs/AB2543/AB2543.svsig.gz"
-sig5="/spaces/lcottino/Pacbio/pbsv/vcfs/DSX0S/DSX0S.svsig.gz"
-sig6="/spaces/lcottino/Pacbio/pbsv/vcfs/AJK0K/AJK0K.svsig.gz"
-sig7="/spaces/lcottino/Pacbio/pbsv/vcfs/AB0330/AB0330.svsig.gz"
-sig8="/spaces/lcottino/Pacbio/pbsv/vcfs/DEC0V/DEC0V.svsig.gz"
-vcf="joint_8_new.vcf"
+ref_seq="Homo_sapiens_assembly38.fasta"
+sig1="CFF0X.svsig.gz"
+sig2="CWF0F.svsig.gz"
+sig3="CNV0L.svsig.gz"
+sig4="AB2543.svsig.gz"
+sig5="DSX0S.svsig.gz"
+sig6="AJK0K.svsig.gz"
+sig7="AB0330.svsig.gz"
+sig8="DEC0V.svsig.gz"
+vcf="joint_8.vcf"
 
 module load smrtlink
 hostname
